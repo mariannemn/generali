0cf3beac5bf28d79c618ae9ff8e4e65ab237b918
mariannemn <mariannemongin@hotmail.fr>
2021-03-12 16:56:49 +0100

    Create dvf_telechargement_import.py

---
+++ dvf_telechargement_import.py
@@ -0,0 +1,51 @@
+# Librairies
+import os
+import urllib.request
+import subprocess
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+
+# Telechargement du fichier compresse 35.csv (DVF)
+filename = '35.csv.gz'
+# ATTENTION - ici il faudrait ajouter le telechargement aussi des annees qui precedent 2020
+url = 'https://cadastre.data.gouv.fr/data/etalab-dvf/latest/csv/2020/departements/' + filename
+destFile = os.path.dirname(__file__) + '/' + filename
+
+urllib.request.urlretrieve(url, destFile) # effectue le telechargement
+
+
+# Decompression du fichier - a voir si possible de l'importer via pandas sans decompresser le fichier
+def extractfiles(zipname, dirPath):
+    cmd = ["C:/Program Files/7-Zip/7z.exe", "-aoa", "-o"+dirPath, "x", zipname]
+    system = subprocess.Popen(cmd)
+    return(system.communicate())
+
+file7zPath = os.path.dirname(__file__) + '/' + "35.csv.gz"
+extractDir = os.path.dirname(__file__) + '/'
+extractfiles(file7zPath, extractDir)
+
+
+# Choix de colonnes à garder dans la table
+columns = ["id_mutation", "date_mutation", "valeur_fonciere", "id_parcelle", "code_type_local", "type_local", "nombre_pieces_principales", "longitude", "latitude"]
+
+# Chargement du CSV en dataframe
+df = pd.read_csv(
+    destFile,
+    usecols=columns,
+    #dtype={"id":"string", "numero":int, "nom_voie":"string", "nom_commune": "string", "lon":float, "lat": float},
+    sep=","
+).replace(to_replace='null', value=np.NaN)
+
+# Instantiation de l'objet sqlalchemy.create_engine
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/generali')
+
+# Import du dataframe dans postgres
+df.to_sql(
+    'dvf',
+    engine,
+    index=False, # Not copying over the index
+    schema='public',
+    if_exists='replace' # if the table already exists, replace this data
+)
