b17151c082520c9e4a86dae5b4ac9da86752b0b9
mariannemn <mariannemongin@hotmail.fr>
2021-04-19 22:08:16 +0200

    modif chemin fichier

--- Telechargement_donnees/bdortho_telechargement.py
+++ Telechargement_donnees/bdortho_telechargement.py
@@ -4,26 +4,12 @@ import subprocess
 import time
 import traceback
 
-# Fonction qui liste les noms de fichiers (connexion FTP)
-def listFileName(con, rep):
-    '''La fonction listFileName(con, rep) prend en entrée une méthode de connection et un répertoire. Elle permet de lister les répertoires contenus.'''
-    cmd = con.cwd(rep)
-    fileList = []
-    cmd = con.dir(fileList.append) # on recupere le listing
-    res = []
-    for l in fileList:
-        t = l.split(' ')
-        res.append(t[-1])
-    return res
+# Import des fichiers de fonctions
+import fonctions_telechargement
+#print(fonctions_telechargement.listFileName.__doc__)
+#print(fonctions_telechargement.extractFiles.__doc__)
 
-
-# Fonction de decompression d'un fichier zip
-def extractFiles(zipname, dirPath):
-    '''La fonction extractFiles permet de dézipper des fichiers 7-Zip.'''
-    cmd = ["C:/Program Files/7-Zip/7z.exe", "-aoa", "-o"+dirPath, "x", zipname]
-    system = subprocess.Popen(cmd)
-    return(system.communicate())
-
+# Informations de connexion FTP
 #ftp://ORTHO_HR_ext:Ithacah6ophai2vo@ftp3.ign.fr/ORTHOHR_1-0_RVB-0M20_JP2-E080_LAMB93_D035_2017-01-01.7z.001
 
 host = "ftp3.ign.fr"            # adresse du serveur FTP
@@ -41,14 +27,14 @@ while (not reussite) and (nb_tentatives <= 10):
 
         ftp = ftplib.FTP(host,user,password) # on se connecte
 
-        dirs = listFileName(ftp,'/')         # listing des repertoires a la racine du ftp
+        dirs = fonctions_telechargement.listFileName(ftp,'/')         # listing des repertoires a la racine du ftp
         print(dirs)
 
         dir35 = list(filter(lambda x: x.find('D035')!=-1, dirs)) # filtrage : on ne prend que le 35
         print(dir35)
 
-# Récupération et écriture du dossier téléchargé
-        f35 = '/' + dirs[-1] + '/' + dir35[0] # chemin du fichier à récupérer sur le ftp
+        # Récupération et écriture du dossier téléchargé
+        f35 = '/' + dir35[0] # chemin du fichier à récupérer sur le ftp
         fdest = os.path.dirname(__file__) + '/' + dir35[0] # chemin du fichier à sauvegarder localement
         print("Getting " + f35 + " --> " + fdest + "...")
 
@@ -66,3 +52,6 @@ while (not reussite) and (nb_tentatives <= 10):
         time.sleep(30) # Temps de latence (30 secondes) après qu'une erreur se soit produite côté serveur
         reussite = False
         
+file7zPath = os.path.dirname(__file__) + '/' + dir35[0]
+extractDir = os.path.dirname(__file__) + '/'
+fonctions_telechargement.extractFiles(file7zPath, extractDir)
